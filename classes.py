760c0f4ea01634f99e1700013fdfcb2aae3108a9
Glenn Lawyer <lawyer@mpi-inf.mpg.de>
2015-12-23 17:11:15 +0100

    ship flies!

--- classes.py
+++ classes.py
@@ -1,4 +1,6 @@
 import pyglet
+import math
+from functions import *
 
 class Portal(pyglet.sprite.Sprite):
   def __init__(self, image, x=0, y=0, batch=None):
@@ -8,15 +10,63 @@ class Portal(pyglet.sprite.Sprite):
 
 
 class Ship(pyglet.sprite.Sprite):
-  def __init__(self, image, x=0, y=0,
-               dx=0, dy=0, rotv=0, batch=None):
+  def __init__(self,eoff_image,eon_image,
+               maxx,maxy,
+               image, x=0, y=0,
+               dx=0, dy=0, rotv=0, batch=None):               
     super(Ship, self).__init__(image, x, y, batch=batch)
+    self.eoff_image=eoff_image
+    self.eon_image=eon_image
+    self.maxx=maxx
+    self.maxy=maxy
     self.x = x
     self.y = y
     self.dx = dx
     self.dy = dy
     self.rotation = rotv
+    self.engines = False
+    self.brakes = False
     self.thrust = 200.0
+    self.friction = 10.0
     self.rot_spd = 100.0
+    self.rot_left = False
+    self.rot_right = False
+
+  def update(self, dt):
+    self.image = self.eoff_image
+    if self.rot_left:
+      self.rotation -= self.rot_spd * dt
+    if self.rot_right:
+      self.rotation += self.rot_spd * dt
+    self.rotation = wrap(self.rotation, 360.0)
+    accelx = math.cos(to_radians(self.rotation))
+    accely = math.sin(to_radians(-self.rotation))
+    ## ## friction slows the ship
+    ## fricx = self.friction * accelx * dt
+    ## fricy = self.friction * accely * dt
+    ## if(self.dx > 0):
+    ##   self.dx -= min(fricx,self.dx)
+    ## else:
+    ##   self.dx += min(fricx,self.dx)
+    ## #if(self.dy > 0):
+    ## #  self.dy -= min(fricy,self.dy)
+    ## #else:
+    ## #  self.dy += min(fricy,self.dy)
+    if self.engines:
+      self.image = self.eon_image
+      self.dx += self.thrust * accelx * dt
+      self.dy += self.thrust * accely * dt
+    if self.brakes:
+      self.dx -= self.thrust * accelx * dt * 0.5
+      self.dy -= self.thrust * accely * dt * 0.5
+    self.x += self.dx *dt
+    self.y += self.dy *dt
+    self.x = wrap(self.x, self.maxx)
+    self.y = wrap(self.y, self.maxy)
+    print("{0:.3f} {1:.3f} {2:.3f} {3:.3f}".format(*(self.dx,self.dy,self.x,self.y)))
 
     
+
+
+
+      
--- space.py
+++ space.py
@@ -1,14 +1,12 @@
 import pyglet
+from pyglet.window import key
 from classes import Portal, Ship
+from functions import *
 
-
-window = pyglet.window.Window(fullscreen=True)
+### Set up main resources (window, images, ...)
+window = pyglet.window.Window(fullscreen=False)
 pyglet.resource.path.append('./resources')
 pyglet.resource.reindex()
-
-def center_anchor(img):
-  img.anchor_x = img.width // 2 ## "//" is the integer division function
-  img.anchor_y = img.height // 2
 
 portal_image = pyglet.resource.image('portal.jpg')
 center_anchor(portal_image)
@@ -16,19 +14,52 @@ center_anchor(portal_image)
 ship_image = pyglet.resource.image('ship.png')
 center_anchor(ship_image)
 
+ship_image_on = pyglet.resource.image('ship_on.png')
+center_anchor(ship_image_on)
+
 center_x = int(window.width/2)
 center_y = int(window.height/2)
 
 portal = Portal(portal_image, center_x, center_y, None)
 
-ship = Ship(ship_image,
-            x=center_x + 300, y = center_y, dx=0, dy=150, rotv=0)
+ship = Ship(eoff_image=ship_image,eon_image=ship_image_on,
+            maxx=window.width,maxy=window.height,
+            image=ship_image,
+            x=center_x + 300, y = center_y, dx=0, dy=100, rotv=-90,
+            batch=None)
 
+def update(dt):
+  ship.update(dt)
+pyglet.clock.schedule_interval(update, 1/60.0)
 
 @window.event
 def on_draw():
   window.clear()
   portal.draw()
   ship.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+  if symbol == key.LEFT:
+    ship.rot_left = True
+  if symbol == key.RIGHT:
+    ship.rot_right = True
+  if symbol == key.UP:
+    ship.engines = True
+  if symbol == key.DOWN:
+    ship.brakes = True
+    
+@window.event
+def on_key_release(symbol, modifiers):
+  if symbol == key.LEFT:
+    ship.rot_left = False
+  if symbol == key.RIGHT:
+    ship.rot_right = False
+  if symbol == key.UP:
+    ship.engines = False
+  if symbol == key.DOWN:
+    ship.brakes = False
+
+  
 
 pyglet.app.run()
